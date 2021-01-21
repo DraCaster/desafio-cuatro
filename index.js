f9a4b7e0319bd4e8114baf37d982690a325296ac
dracaster <lujanrojas.informatica@gmail.com>
2021-01-21 17:47:30 -0300

    subo desafio cuatro

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const PigLatin = function(str){
-    /* YOUR CODE HERE */
-    let result
 
-    return result
+    let splited = str.split("",1)
+    let newString = str.substr(1,str.length)
+
+    switch(splited){
+        case "a": return newString += "way";
+        case "e": return newString += "way";
+        case "i": return newString += "way";
+        case "o": return newString += "way";
+        case "u": return newString += "way";
+        default: return newString += splited + "ay";
+    }
+
+    return newString
 }
 
 module.exports = PigLatin
